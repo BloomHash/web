89cd0c22626709f699870d32a7ca508ed7f12b6b
Freshmilkymilk <fresh@freshy.me>
2020-11-25 15:28:53 -0700

    init of map

---
+++ app/src/Margins/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://github.com/bloomhash/">
+                BloomHash
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const useStyles = makeStyles((theme) => ({
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(6),
+    },
+}));
+
+export default function Footer() {
+    const classes = useStyles();
+
+    return(
+        <footer className={classes.footer}>
+            <Copyright />
+        </footer>
+    );
+}
